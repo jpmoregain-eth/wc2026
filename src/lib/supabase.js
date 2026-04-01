af10c1cc23cd687dd1540178987f53f812d3f649
jpmoregain-eth <jpmoregain-eth@users.noreply.github.com>
2026-04-02 02:01:10 +0800

    Fix Tailwind, add Supabase anon key

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 export const supabase = createClient(
   "https://ttjrzmegmpavhthrvrzb.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR0anJ6bWVnbXBhdmh0aHJ2cnpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQwOTc3OTUsImV4cCI6MjA4OTY3Mzc5NX0.RlBDMFGDKr7Q2u5t9VTQ3Hgf-KtOCGCFoGcK3YRUO9Y"
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR0anJ6bWVnbXBhdmh0aHJ2cnpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQwOTc3OTUsImV4cCI6MjA4OTY3Mzc5NX0.ZEAY5O7i_tBd3PhNlYD42GINnEU6ta27gZCP6N3DNhA"
 )
